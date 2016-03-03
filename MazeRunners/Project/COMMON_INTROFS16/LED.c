17556054e496bfa8795c8b7309c45c4c2f4e72cb
Toretto2015 <cyrill.hefti@stud.hslu.ch>
2016-03-03 09:10:44 +0100

    added Init and Deinit to LED.c

--- MazeRunners/Project/COMMON_INTROFS16/LED.c
+++ MazeRunners/Project/COMMON_INTROFS16/LED.c
@@ -5,4 +5,22 @@
  *      Author: Daniel Föhn
  */
 
+#if PL_NOF_LED
+#include "LED.h"
+
+void LED_Init() {
+#if PL_NOF_LED >= 1
+	LED1_Off();
+#endif
+#if PL_NOF_LED >= 2
+	LED2_Off();
+#endif
+#if PL_NOF_LED >= 3
+	LED3_Off();
+#endif
+}
+
+void LED_Deinit(){
+	LED_Init();
+}
 
