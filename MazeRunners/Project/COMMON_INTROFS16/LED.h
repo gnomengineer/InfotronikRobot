756de7849bb66617571e216c844da1092fa64764
Toretto2015 <cyrill.hefti@stud.hslu.ch>
2016-03-03 08:51:47 +0100

    added LED2 and LED3 to LED.h

--- MazeRunners/Project/COMMON_INTROFS16/LED.h
+++ MazeRunners/Project/COMMON_INTROFS16/LED.h
@@ -19,4 +19,21 @@
 #define LED_Neg(nr) LEDPin##nr##_NegVal();
 #define LED_On()
 
+#if PL_NOF_LED >=2
+#define LED2_On()		LED_On(2);
+#define LED2_Off()		LED_Off(2);
+#define LED2_Neg()		LED_Neg(2);
+#define LED2_Put()		LED_Put(2);
+#define LED2_Get()		LED_Get(2);
+#endif
+
+#if PL_NOF_LED >=3
+#define LED3_On()		LED_On(3);
+#define LED3_Off()		LED_Off(3);
+#define LED3_Neg()		LED_Neg(3);
+#define LED3_Put()		LED_Put(3);
+#define LED3_Get()		LED_Get(3);
+#endif
+
+
 #endif /* SOURCES_COMMON_INTROFS16_LED_H_ */
