775a8f571fdfc0bc8e3d4c538440416d29414527
troopa <troopa.der.pate@gmail>
2016-03-03 08:53:50 +0000

    added methods for LED1 and independant LED methods

--- MazeRunners/Project/COMMON_INTROFS16/LED.c
+++ MazeRunners/Project/COMMON_INTROFS16/LED.c
@@ -2,7 +2,7 @@
  * LED.c
  *
  *  Created on: 1 Mar 2016
- *      Author: troopa
+ *      Author: Daniel Föhn
  */
 
 
--- MazeRunners/Project/COMMON_INTROFS16/LED.h
+++ MazeRunners/Project/COMMON_INTROFS16/LED.h
@@ -2,7 +2,7 @@
  * LED.h
  *
  *  Created on: 1 Mar 2016
- *      Author: troopa
+ *      Author: Daniel Föhn
  */
 
 #ifndef SOURCES_COMMON_INTROFS16_LED_H_
@@ -10,13 +10,31 @@
 
 #if PL_NOF_LED >= 1
 	#include "LEDPin1.h"
+#endif
 #if PL_NOF_LED >= 2
 	#include "LEDPin2.h"
+#endif
 #if PL_NOF_LED >= 3
 	#include "LEDPin3.h"
+#endif
 
 #if PL_NOF_LED >= 1
-#define LED_Neg(nr) LEDPin##nr##_NegVal();
-#define LED_On()
+	#define LED_Neg(nr) LEDPin##nr##_NegVal();
+	#define LED_On(nr) LEDPin##nr##_ClrVal();
+	#define LED_Off(nr) LEDPin##nr##_SetVal();
+	#define LED_Get(nr) LEDPin##nr##_GetVal();
+	#define LED_Put(nr) LEDPin##nr##_PutVal();
+#endif
+
+#if PL_NOF_LED >= 1
+	#define LED1_Neg() LED_Neg(1)
+	#define LED1_On() LED_On(1)
+	#define LED1_Off() LED_Off(1)
+	#define LED1_Get() LED_Get(1)
+	#define LED1_Put() LED_Put(1)
+#endif
+
+void LED_Init(void);
+void LED_Deinit(void);
 
 #endif /* SOURCES_COMMON_INTROFS16_LED_H_ */
