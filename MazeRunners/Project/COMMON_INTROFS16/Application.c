d1584241b2b294eeb68398da1a5f52f067c9bd6a
Toretto2015 <cyrill.hefti@stud.hslu.ch>
2016-03-11 12:02:30 +0100

    updated application.c event led heartbeat is running

--- MazeRunners/Project/COMMON_INTROFS16/Application.c
+++ MazeRunners/Project/COMMON_INTROFS16/Application.c
@@ -3,9 +3,31 @@
 #include "LED.h"
 #include "Event.h"
 
+
 void APP_Start(void) {
   PL_Init();
   /* init: turn off */
+
+  void APP_HandleEvent(EVNT_Handle event)
+  {
+  	switch(event)
+  	{
+  	case EVNT_STARTUP:
+  		//nothing;
+  		break;
+  	case EVENT_LED_HEARTBEAT:
+  		LED1_Neg();
+  		WAIT1_Waitms(500);
+  		break;
+  	case EVENT_SW1_PRESSED:
+  		//do something
+  		break;
+  	default:
+  		//do nothing
+  		break;
+  	}
+  }
+
   EVNT_SetEvent(EVENT_LED_HEARTBEAT);
 
   for(;;) {
@@ -15,26 +37,6 @@ void APP_Start(void) {
   for(;;) {
     /* wait */
   }
-}
-
-void APP_HandleEvent(EVNT_Handle event)
-{
-	switch(event)
-	{
-	case EVNT_STARTUP:
-		//nothing;
-		break;
-	case EVENT_LED_HEARTBEAT:
-		LED1_Neg();
-		WAIT1_Waitms(500);
-		break;
-	case EVENT_SW1_PRESSED:
-		//do something
-		break;
-	default:
-		//do nothing
-		break;
-	}
 }
 
 
