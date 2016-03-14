274ef167e7fd2389c11f56b267766c836496fdba
troopa <troopa.der.pate@gmail>
2016-03-14 11:43:35 +0000

    added Key.c and .h file

---
+++ MazeRunners/Project/COMMON_INTROFS16/Key.c
@@ -0,0 +1,13 @@
+/*
+ * Key.c
+ *
+ *  Created on: 14 Mar 2016
+ *      Author: daniel foehn
+ */
+
+#include "Platform.h"
+
+#if PL_HAS_KEYS
+
+#include "Key.h"
+
---
+++ MazeRunners/Project/COMMON_INTROFS16/Key.h
@@ -0,0 +1,16 @@
+/*
+ * Key.h
+ *
+ *  Created on: 14 Mar 2016
+ *      Author: troopa
+ */
+
+#ifndef SOURCES_COMMON_INTROFS16_KEY_H_
+#define SOURCES_COMMON_INTROFS16_KEY_H_
+
+typedef enum
+{
+
+};
+
+#endif /* SOURCES_COMMON_INTROFS16_KEY_H_ */
