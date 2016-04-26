f33635774477ffffe1592b27cd6c4728d23922aa
troopa <troopa.der.pate@gmail>
2016-04-26 10:47:15 +0200

    enabled LineFollow in platform

--- MazeRunners/Project/COMMON_INTROFS16/Platform.h
+++ MazeRunners/Project/COMMON_INTROFS16/Platform.h
@@ -50,7 +50,7 @@
 #define PL_CONFIG_HAS_PID               (1 && PL_CONFIG_HAS_MOTOR)
 #define PL_CONFIG_HAS_DRIVE             (1 && PL_CONFIG_HAS_MOTOR)
 #define PL_CONFIG_HAS_TURN              (0)
-#define PL_CONFIG_HAS_LINE_FOLLOW       (0)
+#define PL_CONFIG_HAS_LINE_FOLLOW       (1 && PL_CONFIG_HAS_REFLECTANCE)
 #define PL_CONFIG_HAS_RADIO             (0)
 #define RNET_CONFIG_REMOTE_STDIO        (0) /* temporary only, to be remove when RNET gets added */
 #define PL_CONFIG_HAS_REMOTE            (0)
